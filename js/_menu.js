db38eb39e4edd278942bda5516670bfd8808ee4e
Amin Heidarpour <heidarpouramin1379@gmail.com>
2026-04-26 23:28:55 +0200

    ändringar i js/_menu.js

--- js/_menu.js
+++ js/_menu.js
@@ -3,5 +3,6 @@ createMenu("Sverige och valen 2018–2022", [
   { name: "2. Befolkningstäthet", script: "2_density_vote.js" },
   { name: "3. Förändringar", script: "3_shift_between_elections.js" },
   { name: "4. Regioner", script: "4_regional_patterns.js" },
-  { name: "5. Starkast / svagast", script: "5_strongest_weakest.js" }
+  { name: "5. Starkast / svagast", script: "5_strongest_weakest.js" },
+  { name: "6. Källor", script: "6_sources.js" }
 ]);
